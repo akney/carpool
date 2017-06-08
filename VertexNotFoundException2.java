24d183623710507dd974cda127397ebe10d61bd5
Malka S <mschecht4@student.touro.edu>
2017-06-08 16:08:46 -0400

    Add files via upload

---
+++ VertexNotFoundException2.java
@@ -0,0 +1,8 @@
+
+
+public class VertexNotFoundException2 extends Exception {
+
+	public VertexNotFoundException2() {
+		super("The address of the vertex given is not currently in the digraph.");
+	}
+}

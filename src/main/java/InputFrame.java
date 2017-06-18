e6282c17ead09ba4ca80612701f30b6877611de8
Katie Ney <akkn1995@gmail.com>
2017-06-18 13:24:53 -0400

    Simple Gui Main

---
+++ src/main/java/InputFrame.java
@@ -0,0 +1,76 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import com.google.maps.errors.ApiException;
+
+import digraph.Digraph2;
+
+public class InputFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+	JLabel instructions1 = new JLabel("Instructions: Please enter all addresses separated by semicolons.");
+	JLabel instructions2 = new JLabel(
+			"Make sure to list the starting location first and the final destination last. Press calculate when all locations have been entered.");
+	JTextArea input = new JTextArea(5, 20);
+	JScrollPane scroll1 = new JScrollPane(input);
+	JButton calculate = new JButton("Calculate");
+	JLabel replyLabel = new JLabel("Shortest Route: ");
+	JTextArea reply = new JTextArea(5, 20);
+	JScrollPane scroll2 = new JScrollPane(reply);
+
+	public InputFrame() {
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setSize(800, 600);
+		this.setTitle("QuickPool - The fastest way to do carpool!");
+		this.setVisible(true);
+
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+
+		add(instructions1);
+		add(instructions2);
+
+		add(scroll1);
+		add(calculate);
+		add(replyLabel);
+		reply.setEditable(false);
+		add(scroll2);
+
+		calculate.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String locs = input.getText();
+				if (!locs.trim().equals("")) {
+					String[] locations = locs.split(";");
+
+					Digraph2 graph;
+					try {
+						graph = new Digraph2(locations);
+						// find shortest route from starting point to final
+						// destination
+						// going
+						// through all points
+						graph.PrimsAlgorithm();
+
+						reply.setText(graph.displayLocationsInOrder());
+					} catch (ApiException | InterruptedException | IOException e) {
+						JOptionPane.showMessageDialog(null, "Sorry - there was a problem with our maps connection.");
+						e.printStackTrace();
+					}
+				}
+
+			}
+
+		});
+
+	}
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,64 +1,6 @@
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-import com.google.maps.errors.ApiException;
-
-import digraph.Digraph2;
-
 public class Main {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-
-		try {
-			int choice = 0;
-			do {
-
-				choice = menu();
-
-				switch (choice) {
-				case 1:
-					System.out.println(
-							"Instructions: Please enter all addresses separated by semicolons.  \nMake sure to list the starting location first and the final destination last. Press enter when all locations have been entered.");
-
-					String locs = input.nextLine();
-
-					String[] locations = locs.split(";");
-
-					Digraph2 graph = new Digraph2(locations);
-
-					// find shortest route from starting point to final
-					// destination
-					// going
-					// through all points
-					graph.PrimsAlgorithm();
-
-					System.out.println("Shortest Route: " + graph.displayLocationsInOrder());
-					break;
-				default:
-					System.out.println("Good Bye!");
-				}
-
-			} while (choice != 2);
-		} catch (ApiException | InterruptedException | IOException e) {
-			System.out.println("Sorry - there was a problem with our maps connection.");
-		} catch (InputMismatchException e) {
-			System.out.println("Invalid Input.");
-		}
-
-	}
-
-	public static int menu() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("\n1. Enter new locations" + "\n2. Exit");
-
-		int choice = input.nextInt();
-
-		while (choice < 1 || choice > 2) {
-			System.out.println("Invalid Input.  Please choose a menu item.");
-			choice = input.nextInt();
-		}
-
-		return choice;
+		InputFrame input = new InputFrame();
+		input.setVisible(true);
 	}
 }
